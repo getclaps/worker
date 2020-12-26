09b2fa7483533717791a48d7095655e0753e0e68
Florian Klampfer <mail@qwtel.com>
2020-12-26 15:43:38 +0700

    Session fixes

--- src/routes/session.ts
+++ src/routes/session.ts
@@ -1,38 +1,46 @@
 import { UUID } from 'uuid-class';
 
-import { JSONObject } from '../vendor/json-types';
-import { KV as database } from '../constants';
+import { JSONObject, JSONValue } from '../vendor/json-types';
 import { compressId, elongateId } from '../short-id';
 
 import { Cookies, mkSessionCookie } from './cookies';
 
-// TODO: make configurable: key, KV database, duration
-const SESSION_KEY = 'ASP.NET_SessionId';
-
-const getSessionObj = async (cookies: Map<string, string>): Promise<[UUID, JSONObject]> => {
-  const sid = new UUID(elongateId(cookies.get(SESSION_KEY)));
-  const sessionObj: JSONObject = await database.get(sid.id, 'json');
-  return sessionObj != null ? [sid, sessionObj] : [new UUID(), {}];
+const getSessionObj = async (kv: KVNamespace, sessionKey: string, cookies: Cookies): Promise<[UUID, JSONObject]> => {
+  const sid = new UUID(elongateId(cookies.get(sessionKey)));
+  const sessionObj: JSONObject = await kv.get(sid.id, 'json');
+  return sessionObj 
+    ? [sid, sessionObj] 
+    : [new UUID(), {}];
 }
 
-export const withSession = <T extends { event: FetchEvent, cookies: Cookies }>(handler: (args: T & { session: JSONObject }) => Promise<Response>) => async (args: T): Promise<Response> => {
-  const [sid, sessionObj] = await getSessionObj(args.cookies);
-
-  let tid: number;
-  const session = new Proxy(sessionObj, {
-    set(target, prop, v) {
-      Reflect.set(target, prop, v);
-
-      clearInterval(tid);
-      tid = setTimeout(() => args.event.waitUntil(
-        database.put(sid.id, JSON.stringify(target), { expirationTtl: 5 * 60 })
-      ), 10);
-
-      return true;
-    }
-  })
-
-  const response = await handler({ ...args, session });
-  response.headers.append('set-cookie', mkSessionCookie(SESSION_KEY, compressId(sid)));
-  return response;
+interface SessionOptions {
+  kv: KVNamespace,
+  sessionKey?: string,
+  expirationTtl?: number,
 }
+
+const coerceJSONVal = (v: JSONValue) => JSON.parse(JSON.stringify(v));
+
+export const withSession = ({ kv, sessionKey = 'sid', expirationTtl = 5 * 60 }: SessionOptions) => 
+  <T extends { event: FetchEvent, cookies: Cookies }>(handler: (args: T & { session: JSONObject }) => Promise<Response>) => 
+    async (args: T): Promise<Response> => {
+      const [sid, sessionObj] = await getSessionObj(kv, sessionKey, args.cookies);
+
+      let tid: number;
+      const session = new Proxy(sessionObj, {
+        set(target, prop, v) {
+          Reflect.set(target, prop, coerceJSONVal(v));
+
+          clearInterval(tid);
+          tid = setTimeout(() => args.event.waitUntil(
+            kv.put(sid.id, JSON.stringify(target), { expirationTtl })
+          ), 10);
+
+          return true;
+        }
+      })
+
+      const response = await handler({ ...args, session });
+      response.headers.append('set-cookie', mkSessionCookie(sessionKey, compressId(sid)));
+      return response;
+    };
