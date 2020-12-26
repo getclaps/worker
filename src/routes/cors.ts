3445dcbc20149b062445a211176b360bbe39f843
Florian Klampfer <mail@qwtel.com>
2020-12-26 13:10:30 +0700

    Update cors

--- src/routes/cors.ts
+++ src/routes/cors.ts
@@ -1,12 +1,17 @@
 import { Awaitable } from "../router";
 
+const ORIGIN = 'origin';
+const METHOD = 'access-control-request-method';
+const HEADERS = 'access-control-request-headers';
+
 export const withCORS = <T extends { event: FetchEvent }>(handler: (args: T) => Awaitable<Response>) => async (args: T): Promise<Response> => {
-  const request = args.event.request;
+  const { headers } = args.event.request;
+
   const response = await handler(args);
 
-  response.headers.set('access-control-allow-origin', request.headers.get('origin'));
-  if (request.headers.has('access-control-request-method')) response.headers.set('access-control-allow-methods', request.headers.get('access-control-request-method'));
-  if (request.headers.has('access-control-request-headers')) response.headers.set('access-control-allow-headers', request.headers.get('access-control-request-headers'));
+  response.headers.set('access-control-allow-origin', headers.get(ORIGIN));
+  if (headers.has(METHOD)) response.headers.set('access-control-allow-methods', headers.get(METHOD));
+  if (headers.has(HEADERS)) response.headers.set('access-control-allow-headers', headers.get(HEADERS));
   response.headers.set('access-control-allow-credentials', 'true');
 
   return response;
