89bbe7697c52978dad0f9bb984c904cf21f4ae67
Florian Klampfer <mail@qwtel.com>
2020-12-26 16:53:50 +0700

    Update session

--- src/routes/session.ts
+++ src/routes/session.ts
@@ -1,9 +1,13 @@
 import { UUID } from 'uuid-class';
 
-import { JSONObject, JSONValue } from '../vendor/json-types';
+import { JSONValue } from '../vendor/json-types';
 import { compressId, elongateId } from '../short-id';
 
 import { Cookies, mkSessionCookie } from './cookies';
+import { Awaitable } from '../router';
+
+type Args = { event: FetchEvent, cookies: Cookies };
+type WithSessionHandler<T> = (args: T & { session: Session }) => Awaitable<Response>
 
 interface SessionOptions {
   kv: KVNamespace,
@@ -11,38 +15,43 @@ interface SessionOptions {
   expirationTtl?: number,
 }
 
-const coerceJSONVal = (v: JSONValue) => JSON.parse(JSON.stringify(v));
+const coerceJSON = (v: JSONValue) => JSON.parse(JSON.stringify(v));
 
-class Session extends Map<string, JSONValue> {
+const create = Symbol('Session.create');
+
+export class Session extends Map<string, JSONValue> {
   id: UUID
 
-  #kv: KVNamespace
-  #tid: number;
-  #event: FetchEvent;
-  #expirationTtl: number;
+  private event: FetchEvent;
+  private kv: KVNamespace
+  private expirationTtl: number;
 
-  static async from(cookies: Cookies, event: FetchEvent, { kv, sessionKey = 'sid', expirationTtl = 5 * 60 }: SessionOptions) {
+  private static async [create]({ event, cookies }: Args, { kv, sessionKey, expirationTtl }: SessionOptions) {
     let sid = new UUID(elongateId(cookies.get(sessionKey)));
-    const obj: any = await kv.get(sid.id, 'json');
+    const obj = await kv.get(sid.id, 'json') as any;
     sid = obj != null ? sid : new UUID();
-    return new Session(obj ?? {}, sid, kv, event, expirationTtl);
+    return new Session(create, obj ?? {}, sid, event, { kv, expirationTtl });
   }
 
-  private constructor(obj: object, id: UUID, kv: KVNamespace, event: FetchEvent, expirationTtl: number) {
+  private constructor(caller: symbol, obj: object, id: UUID, event: FetchEvent, { kv, expirationTtl }: SessionOptions) {
+    if (caller !== create) throw Error('Illegal constructor');
     super(Object.entries(obj));
     this.id = id;
-    this.#kv = kv;
-    this.#event = event;
-    this.#expirationTtl = expirationTtl;
+    this.event = event;
+    this.kv = kv;
+    this.expirationTtl = expirationTtl;
   }
 
-  set(key: string, value: JSONValue): this {
-    super.set(key, coerceJSONVal(value));
+  private tid: number;
 
-    clearInterval(this.#tid);
-    this.#tid = setTimeout(() => this.#event.waitUntil(
-      this.#kv.put(this.id.id, JSON.stringify(this), { expirationTtl: this.#expirationTtl })
-    ), 10);
+  set(key: string, value: JSONValue): this {
+    super.set(key, coerceJSON(value));
+
+    // Batch calls within the same micro task:
+    clearInterval(this.tid);
+    this.tid = setTimeout(() => this.event.waitUntil(
+      this.kv.put(this.id.id, JSON.stringify(this), { expirationTtl: this.expirationTtl })
+    ));
 
     return this;
   }
@@ -52,11 +61,11 @@ class Session extends Map<string, JSONValue> {
   }
 }
 
-export const withSession = (opts: SessionOptions) => 
-  <T extends { event: FetchEvent, cookies: Cookies }>(handler: (args: T & { session: Session }) => Promise<Response>) => 
+export const withSession = ({ kv, sessionKey = 'sid', expirationTtl = 5 * 60 }: SessionOptions) => 
+  <T extends Args>(handler: WithSessionHandler<T>) => 
     async (args: T): Promise<Response> => {
-      const session = await Session.from(args.cookies, args.event, opts)
+      const session = await Session[create](args, { kv, sessionKey, expirationTtl })
       const response = await handler({ ...args, session });
-      response.headers.append('set-cookie', mkSessionCookie(opts.sessionKey, compressId(session.id)));
+      response.headers.append('set-cookie', mkSessionCookie(sessionKey, compressId(session.id)));
       return response;
     };
