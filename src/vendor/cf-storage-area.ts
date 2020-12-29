909024a18d93f405d17f15a2fbc813e192212da4
Florian Klampfer <mail@qwtel.com>
2020-12-29 19:56:38 +0700

    Update cf storage area

--- src/vendor/cf-storage-area.ts
+++ src/vendor/cf-storage-area.ts
@@ -1,18 +1,18 @@
 import Typeson from 'typeson';
 import structuredCloningThrowing from 'typeson-registry/dist/presets/structured-cloning-throwing';
-import { encodeKey, decodeKey } from './cf-storage-area-keys';
 
-import { Key, StorageArea, throwForDisallowedKey } from './storage-area';
+import { Awaitable } from './common-types';
+import { encodeKey, decodeKey, throwForDisallowedKey } from './cf-storage-area-keys';
+import { Key, StorageArea } from './storage-area';
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm
 const TSON = new Typeson().register(structuredCloningThrowing);
 
-// TODO: options to use BSON or other binary rep instead of JSON.stringify...
-const setValue = <T>(kv: KVNamespace, key: string, value: T, options?: CFSetOptions) =>
-  kv.put(key, JSON.stringify(TSON.encapsulate(value)), options);
+const setValue = async <T>(kv: KVNamespace, key: string, value: T, options: CFSetOptions, packer: KVPacker) =>
+  kv.put(key, await packer.pack(TSON.encapsulate(value)), options);
 
-const getValue = async (kv: KVNamespace, key: string) =>
-  TSON.revive(await kv.get(key, 'json'));
+const getValue = async (kv: KVNamespace, key: string, packer: KVPacker) =>
+  TSON.revive(await packer.unpack(kv, key));
 
 async function* paginationHelper(kv: KVNamespace) {
   let keys: { name: string; expiration?: number; metadata?: unknown }[];
@@ -29,26 +29,37 @@ export interface CFSetOptions {
   expirationTtl?: string | number;
 }
 
+/**
+ * An implementation of the `StorageArea` interface, wrapping Cloudflare Worker's KV store.
+ * 
+ * The goal of this class is ease of use and compatibility with other Storage Area implementations, 
+ * such as <https://github.com/GoogleChromeLabs/kv-storage-polyfill>.
+ * 
+ * While work on [the specification](https://wicg.github.io/kv-storage/) itself has stopped, 
+ * it's still a good interface for asynchronous data access that feels native to JavaScript.
+ */
 export class CFStorageArea implements StorageArea<KVNamespace> {
   #kv: KVNamespace;
+  #packer: KVPacker;
 
-  constructor(name: string | KVNamespace) {
+  constructor(name: string | KVNamespace, { packer = new JSONPacker() }: { packer?: KVPacker } = {}) {
     this.#kv = (typeof name === 'string')
       ? Reflect.get(self, name)
       : name;
     if (!this.#kv) throw Error('KV binding missing. Consult CF Workers documentation for details')
+    this.#packer = packer;
   }
 
   async get<T>(key: Key): Promise<T> {
     throwForDisallowedKey(key);
-    return getValue(this.#kv, encodeKey(key));
+    return getValue(this.#kv, encodeKey(key), this.#packer);
   }
 
   async set<T>(key: Key, value: T | undefined, options?: CFSetOptions): Promise<void> {
     if (value === undefined) await this.#kv.delete(encodeKey(key));
     else {
       throwForDisallowedKey(key);
-      await setValue(this.#kv, encodeKey(key), value, options);
+      await setValue(this.#kv, encodeKey(key), value, options, this.#packer);
     }
   }
 
@@ -71,13 +82,13 @@ export class CFStorageArea implements StorageArea<KVNamespace> {
 
   async *values<T>(): AsyncGenerator<T> {
     for await (const key of paginationHelper(this.#kv)) {
-      yield getValue(this.#kv, key);
+      yield getValue(this.#kv, key, this.#packer);
     }
   }
 
   async *entries<T, K extends Key = Key>(): AsyncGenerator<[K, T]> {
     for await (const key of paginationHelper(this.#kv)) {
-      yield [decodeKey(key), await getValue(this.#kv, key)];
+      yield [decodeKey(key), await getValue(this.#kv, key, this.#packer)];
     }
   }
 
@@ -85,3 +96,19 @@ export class CFStorageArea implements StorageArea<KVNamespace> {
     return this.#kv;
   }
 }
+
+export interface KVPacker {
+  pack(x: any): Awaitable<string | ArrayBuffer | ReadableStream<Uint8Array>>;
+  unpack(kv: KVNamespace, key: string): Promise<any>;
+}
+
+export class JSONPacker implements KVPacker {
+  pack(x: any) { 
+    return JSON.stringify(x);
+  }
+  async unpack(kv: KVNamespace, key: string) { 
+    return await kv.get(key, 'json');
+  }
+}
+
+export * from './storage-area';
--- src/vendor/storage-area.ts
+++ src/vendor/storage-area.ts
@@ -1,51 +1,26 @@
 export type Key = any;
+type Options = Record<string, any>;
 
 declare var StorageArea: {
     prototype: StorageArea;
     new(name: string): StorageArea;
 };
 
+/**
+ * Main differences to the working draft:
+ * - Type parameter for the backing store.
+ * - Added unspecified options paramter to all methods. Implementations can 
+ *   It's unlikely that the working draft will add parameters to the methods, and if they do your keys might not interfere.
+ */
 export interface StorageArea<BS = any> {
-  set<T>(key: Key, value: T, opts?: Record<string, any>): Promise<void> ;
-  get<T>(key: Key, opts?: Record<string, any>): Promise<T> ;
-  delete(key: Key): Promise<void> ;
-  clear(): Promise<void> ;
+  set<T>(key: Key, value: T, opts?: Options): Promise<void> ;
+  get<T>(key: Key, opts?: Options): Promise<T> ;
+  delete(key: Key, opts?: Options): Promise<void> ;
+  clear(opts?: Options): Promise<void> ;
 
-  keys(): AsyncIterableIterator<Key>;
-  values<T>(): AsyncIterableIterator<T>;
-  entries<T>(): AsyncIterableIterator<[Key, T]>;
+  keys(opts?: Options): AsyncIterableIterator<Key>;
+  values<T>(opts?: Options): AsyncIterableIterator<T>;
+  entries<T>(opts?: Options): AsyncIterableIterator<[Key, T]>;
 
   backingStore(): BS;
 };
-
-export function throwForDisallowedKey(key: any) {
-  if (!isAllowedAsAKey(key)) {
-    throw Error('kv-storage: The given value is not allowed as a key: ' + key);
-  }
-}
-
-function isAllowedAsAKey(value: any) {
-  if (typeof value === 'number' || typeof value === 'string') {
-    return true;
-  }
-
-  if (typeof value === 'object' && value) {
-    if (Array.isArray(value)) {
-      return true;
-    }
-
-    if (value instanceof Date) {
-      return true;
-    }
-
-    if (ArrayBuffer.isView(value)) {
-      return true;
-    }
-
-    if (value instanceof ArrayBuffer) {
-      return true;
-    }
-  }
-
-  return false;
-}
