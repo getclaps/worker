61ba18ce29a39290547396540dda9225ef97737a
Florian Klampfer <mail@qwtel.com>
2021-01-02 16:15:59 +0700

    Add support for stateless sessions

--- src/vendor/middleware/session.ts
+++ src/vendor/middleware/session.ts
@@ -1,16 +1,24 @@
+import 'abortcontroller-polyfill';
+
 import { StorageArea } from '@werker/cloudflare-kv-storage';
 import { UUID } from 'uuid-class';
+// import { Packr } from 'msgpackr/browser';
+import { Encoder as CBOREncoder, Decoder as CBORDecoder } from 'cbor-x/browser';
 
 import { BaseArg, Handler } from '.';
 import { Awaitable } from '../common-types';
 import { WithCookiesArgs } from './cookies';
 import { shortenId, parseUUID } from '../short-id';
+import { Base64Decoder, Base64Encoder } from 'base64-encoding';
+
+type AnyRec = Partial<Record<any, any>>;
 
 export type WithSessionDeps = BaseArg & WithCookiesArgs;
 export type WithSessionArgs<S extends AnyRec = AnyRec> = { session: S };
 export type WithSessionHandler<A extends WithSessionDeps, S> = (args: A & WithSessionArgs<S>) => Awaitable<Response>;
 
-type AnyRec = Record<any, any>;
+const stringifySessionCookie = <T>(value: T) => new Base64Encoder({ url: true }).encode(new CBOREncoder({ structuredClone: true }).encode(value));
+const parseSessionCookie = <T>(value: string) => <T>new CBORDecoder({ structuredClone: true }).decode(new Base64Decoder().decode(value));
 
 export interface SessionOptions {
   /** The storage area where to persist the session objects */
@@ -36,28 +44,53 @@ export interface SessionOptions {
  * Issues
  * - Will "block" until session object is retrieved from KV => provide "unyielding" version that returns a promise?
  */
-export const withSession = <S extends AnyRec = AnyRec>({ storage, cookieName = 'sid', expirationTtl = 5 * 60 }: SessionOptions) =>
+export const withSession = <S extends AnyRec = AnyRec>({ storage, cookieName = 'sidcborx', expirationTtl = 5 * 60 }: SessionOptions = {}) =>
   <A extends WithSessionDeps>(handler: WithSessionHandler<A, S>): Handler<A> =>
     async (args: A): Promise<Response> => {
       const { cookies, cookieStore, event } = args;
 
-      const sessionId = parseUUID(cookies.get(cookieName)) ?? new UUID();
-      const session = await getSessionObject<S>(sessionId, event, { storage, cookieName, expirationTtl });
+      const ac = new AbortController();
+
+      const [id, session] = await getSessionObject<S>(cookies.get(cookieName), event, { storage, cookieName, expirationTtl }, ac.signal);
 
       const response = await handler({ ...args, session });
 
       await cookieStore.set({
         name: cookieName,
-        value: shortenId(sessionId),
+        value: storage ? shortenId(id) : stringifySessionCookie(session),
         sameSite: 'lax',
         httpOnly: true,
         expires: null, // session cookie
       });
 
+      // Indicate that cookie session can no longer be modified.
+      ac.abort();
+
       return response;
     };
 
-async function getSessionObject<S extends AnyRec = AnyRec>(sessionId: UUID, event: FetchEvent, { storage, expirationTtl }: SessionOptions): Promise<S> {
+async function getSessionObject<S extends AnyRec = AnyRec>(cookieVal: string, event: FetchEvent, { storage, expirationTtl }: SessionOptions, signal: AbortSignal): Promise<[UUID|null, S]> {
+  if (!storage) {
+    const obj = cookieVal && parseSessionCookie<S>(cookieVal) || <S>{};
+
+    return [null, new Proxy(<any>obj, {
+      set(target, prop, value) {
+        if (signal.aborted)
+          throw Error('Headers already sent, session can no longer be modified!');
+        target[prop] = value;
+        return true;
+      },
+
+      deleteProperty(target, prop) {
+        if (signal.aborted)
+          throw Error('Headers already sent, session can no longer be modified!');
+        delete target[prop];
+        return true;
+      }
+    })];
+  }
+    
+  const sessionId = parseUUID(cookieVal) ?? new UUID();
   const obj = (await storage.get<S>(sessionId)) || <S>{};
 
   let nr = 0;
@@ -72,7 +105,7 @@ async function getSessionObject<S extends AnyRec = AnyRec>(sessionId: UUID, even
     })());
   }
 
-  return new Proxy(<any>obj, {
+  return [sessionId, new Proxy(<any>obj, {
     set(target, prop, value) {
       target[prop] = value;
       persist();
@@ -84,5 +117,5 @@ async function getSessionObject<S extends AnyRec = AnyRec>(sessionId: UUID, even
       persist();
       return true;
     }
-  });
+  })];
 }
