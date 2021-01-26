d9f33f0b17b544650e37f3eb06ee4fb6dce86766
Florian Klampfer <mail@qwtel.com>
2021-01-26 11:47:16 +0700

    Remove unused

--- src/vendor/json-types.ts
+++
@@ -1,5 +0,0 @@
-export type JSONable = { toJSON: (k?: string) => JSONValue }
-export type JSONPrimitive = string | number | boolean | null;
-export type JSONValue = JSONPrimitive | JSONObject | JSONArray | JSONable;
-export type JSONObject = { [k: string]: JSONValue };
-export type JSONArray = JSONValue[];
