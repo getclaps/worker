d3516cce27a33b98c54dbddcabbe382d242d5c71
Florian Klampfer <mail@qwtel.com>
2021-03-06 14:22:57 +0700

    Fix deno env

--- deno-env.js
+++ deno-env.js
@@ -1,3 +1,2 @@
 export let AUTH = 'Basic YWRtaW46cGFzc3dvcmQ'; // btoa('admin:password') 
-export let WORKER_DOMAIN = 'localhost:8000';
-export let DENO_STORAGE_AREA__DEFAULT_URI = 'sqlite://database.sqlite';
+self.DENO_STORAGE_AREA__DEFAULT_URI = 'sqlite://database.sqlite';
