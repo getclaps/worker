9506eb023bfa2aa1ac424c5f8dd7ce1f9da3a101
Florian Klampfer <mail@qwtel.com>
2021-03-08 12:45:33 +0700

    Fixes

--- deno-env.js
+++ deno-env.js
@@ -1,2 +1,2 @@
 export let AUTH = 'Basic YWRtaW46cGFzc3dvcmQ'; // btoa('admin:password') 
-self.DENO_STORAGE_AREA__DEFAULT_URI = 'sqlite://database.sqlite';
+self.DENO_STORAGE_AREA__DEFAULT_URL = 'sqlite://database.sqlite';
